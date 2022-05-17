a9405ab67a069372a427576dcc4ce80bad2403ca
IvanJR18 <ivanjimenezrui01@gmail.com>
2022-05-17 06:44:51 -0400

    Message

---
+++ examen_back_end/src/main/java/com/example/examen_front_end/ExamenFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.examen_front_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFrontEndApplication.class, args);
+    }
+
+}
